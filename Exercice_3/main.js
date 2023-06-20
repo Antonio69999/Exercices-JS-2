753354f8bf16e86e69d2e40004c5d0e0a8530b9e
Antonio69999 <antoine.chabeauti@gmail.com>
2023-06-20 14:59:39 +0200

    exercice_4

---
+++ Exercice_3/main.js
@@ -0,0 +1,5 @@
+document.getElementById("lastname").addEventListener("keydown", keydown_function);
+
+function keydown_function() {
+    alert("input");
+}
---
+++ Exercice_4/main.js
@@ -0,0 +1,3 @@
+function del_form() {
+    document.getElementById("forms").reset();
+}
